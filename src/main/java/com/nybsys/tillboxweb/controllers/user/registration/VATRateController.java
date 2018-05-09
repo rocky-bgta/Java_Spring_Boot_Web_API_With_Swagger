47c84503e27786bedaeccff4b8cfc9c0d89b7dfb
rocky-bgta <rocky.bgta@gmail.com>
2018-05-09 14:59:31 +0600

    country vat

--- src/main/java/com/nybsys/tillboxweb/controllers/user/registration/VATRateController.java
+++ src/main/java/com/nybsys/tillboxweb/controllers/user/registration/VATRateController.java
@@ -8,50 +8,75 @@
  */
 package com.nybsys.tillboxweb.controllers.user.registration;
 
+import com.nybsys.tillboxweb.broker.client.MqttCallBack;
+import com.nybsys.tillboxweb.broker.client.MqttUtils;
+import com.nybsys.tillboxweb.core.Core;
 import com.nybsys.tillboxweb.core.RequestMessage;
 import com.nybsys.tillboxweb.core.ResponseMessage;
 import com.nybsys.tillboxweb.models.user.registration.VATRateModel;
 import io.swagger.annotations.ApiOperation;
+import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.UUID;
+
+import static com.nybsys.tillboxweb.core.Core.QoS;
+
 @RestController
 @RequestMapping("api/vatRate/")
 
 public class VATRateController {
 
-   /* @Autowired
-    MqttSubscribePublished mqttSubscribePublished;*/
-/*
-    @ApiOperation(value = UserRegistrationModuleConstant.VAT_RATE_SAVE_SERVICE_LINK_DESCRIPTION, response = VATRateModel.class)
-    @RequestMapping(value = UserRegistrationModuleConstant.SAVE_SERVICE_LINK, method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseMessage save(@RequestBody RequestMessage requestMessage) throws MqttException {
-        ResponseMessage responseMessage = null;
+    final static String vatPublishedTopic="vatPublishedTopic";
+    final static String vatSubscriptionTopic="vatSubscriptionTopic";
+    private static MqttClient mqttClient;
+
+    static {
         try {
-            //this.mqttSubscribePublished.setSubscriptionTopic(WEB_API_USER_REGISTRATION_TOPIC);
-            //this.mqttSubscribePublished.setPublishedTopic(WORKER_USER_REGISTRATION_MODULE_TOPIC);
-            //responseMessage = mqttSubscribePublished.getResponseMessage(UserRegistrationModuleConstant.VAT_RATE_BASE_URL+UserRegistrationModuleConstant.SAVE_SERVICE_LINK, requestMessage);
-        } catch (Exception e) {
-            e.printStackTrace();
+            mqttClient = MqttUtils.getMqttClient();
+        }catch (Exception ex){
+            ex.printStackTrace();
         }
-        return responseMessage;
-    }*/
+    }
 
     @ApiOperation(value = "", response = VATRateModel.class)
     @RequestMapping(value = "getAll", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseMessage search(@RequestBody RequestMessage requestMessage) throws MqttException {
         ResponseMessage responseMessage = null;
+        String jsonString;
         try {
-            //this.mqttSubscribePublished.setSubscriptionTopic(WEB_API_USER_REGISTRATION_TOPIC);
-            //this.mqttSubscribePublished.setPublishedTopic(WORKER_USER_REGISTRATION_MODULE_TOPIC);
-            //responseMessage = mqttSubscribePublished.getResponseMessage(UserRegistrationModuleConstant.VAT_RATE_BASE_URL+UserRegistrationModuleConstant.SEARCH_SERVICE_LINK, requestMessage);
-        } catch (Exception e) {
-            e.printStackTrace();
+
+            Object lock = new Object();
+            MqttCallBack mqttCallBack = new MqttCallBack();
+            mqttCallBack.setLock(lock);
+            mqttClient.setCallback(mqttCallBack);
+            mqttClient.subscribe(vatSubscriptionTopic,QoS);
+
+            String uuid = UUID.randomUUID().toString();
+
+            MqttMessage mqttMessage;
+            mqttMessage = MqttUtils.getMqttDefaultMessage();
+            requestMessage.token = uuid;
+            jsonString = Core.jsonMapper.writeValueAsString(requestMessage);
+            mqttMessage.setPayload(jsonString.getBytes());
+            mqttClient.publish(vatPublishedTopic,mqttMessage);
+
+            synchronized (lock){
+                lock.wait();
+            }
+
+            responseMessage = mqttCallBack.getResponseMessage();
+
+        }catch (Exception ex){
+            ex.printStackTrace();
         }
+
         return responseMessage;
     }
 
